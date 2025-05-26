f5053509564de2632601f6cd732373af9958e7c0
Matic Sisko Musil <130707864+MaticSM1@users.noreply.github.com>
2025-05-26 15:00:07 +0200

    podlaga za app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(__dirname + '/main.html');
 });
 
 app.listen(port, () => {
