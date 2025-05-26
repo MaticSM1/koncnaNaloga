b26720669f6683135e654d6a65db40e364c49f5f
Matic Sisko Musil <130707864+MaticSM1@users.noreply.github.com>
2025-05-26 16:26:54 +0200

    nastavitve ping button

--- app/app/src/main/java/com/example/myapplication/Settings.kt
+++ app/app/src/main/java/com/example/myapplication/Settings.kt
@@ -1,15 +1,29 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class Settings : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
         enableEdgeToEdge()
+
+        val pingButton = findViewById<Button>(R.id.ping)
+        pingButton.setOnClickListener {
+
+        }
+
         setContentView(R.layout.activity_settings)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
