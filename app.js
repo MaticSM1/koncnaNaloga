23e36009fc3920abcad1791550e67ffcb0ea56b0
Matic Sisko Musil <130707864+MaticSM1@users.noreply.github.com>
2025-05-26 20:38:47 +0200

    register

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ async function run() {
     try {
         await client.connect();
         await client.db("admin").command({ ping: 1 });
-    } finally {
-        await client.close();
+        console.log("MongoDB connected!");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
     }
 }
 run().catch(console.dir);
@@ -32,8 +33,10 @@ run().catch(console.dir);
 //! EXPRESS  
 app.use(express.json());
 
+app.use('/public', express.static(__dirname + '/sites/public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/main.html');
+    res.sendFile(__dirname + '/sites/main.html');
 });
 
 app.get('/ping', (req, res) => {
@@ -44,6 +47,48 @@ app.get('/ping', (req, res) => {
 app.listen(port, () => {
     console.log(`Server na http://localhost:${port}`);
 });
+
+
+//! REGISTER
+
+
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password required' });
+    }
+    try {
+        const db = client.db('users');
+        const existingUser = await db.collection('users').findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ message: 'Email already exists' });
+        }
+        await db.collection('users').insertOne({ email, password });
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) {
+        console.error('Error during registration:', err);
+        res.status(500).json({ message: 'Server error', error: err.message });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const db = client.db('users');
+        const user = await db.collection('users').findOne({ username });
+        if (user && user.password === password) {
+            res.status(200).json({ message: 'Login successful' });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Server error', error: err.message });
+    }
+});
+
+
+
+//! MQTT
 
 
 const mqttSettings = {
