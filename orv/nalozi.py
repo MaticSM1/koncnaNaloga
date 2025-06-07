fc758de7a138456b1663df3bab7f431f533492ca
Matic Sisko Musil <130707864+MaticSM1@users.noreply.github.com>
2025-06-07 19:22:42 +0200

    prenos slik iz serverja

---
+++ orv/nalozi.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from PIL import Image
+from io import BytesIO
+
+os.makedirs("input", exist_ok=True)
+
+for i in range(20):
+    url = f"https://z7.si/wisfi/orvinput/{i}.jpg"
+    output_path = f"input/{i}.jpg"
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(output_path, "wb") as f:
+            f.write(response.content)
+        with Image.open(output_path) as img:
+            width = 800
+            ratio = width / img.width
+            height = int(img.height * ratio)
+            img = img.resize((width, height), Image.LANCZOS)
+            img.save(output_path)
+        print(f"Slika {i}.jpg snajena {output_path}")
+    else:
+   
