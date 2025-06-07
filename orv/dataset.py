369172b0faf7aa6420d6f025582434c2c411d5dd
DavidPilih <davidpilih4242@gmail.com>
2025-06-07 21:22:02 +0200

    face recognition test implemented

--- orv/dataset.py
+++ orv/dataset.py
@@ -4,9 +4,9 @@ from torch.utils.data import DataLoader
 
 def get_data_loaders(data_dir, batch_size=32, input_size=224):
     transform = transforms.Compose([
-        transforms.Resize((input_size, input_size)),
+        transforms.Resize((input_size, input_size)), 
         transforms.ToTensor(),
-        transforms.Normalize([0.5], [0.5])
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
     ])
 
     train_path = os.path.join(data_dir, "train")
