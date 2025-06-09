92a0126981aa9076594dba169c3f1600bd865853
DavidPilih <davidpilih4242@gmail.com>
2025-06-09 18:20:40 +0200

    user model updated

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         sparse: true
     },
+    products: [{
+        type: String,
+        ref: 'Product'
+    }]
 });
 
 module.exports = mongoose.model('User', UserSchema);
