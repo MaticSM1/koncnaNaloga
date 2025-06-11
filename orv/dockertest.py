baed8a5c723b9c9128e74e90bb92c7329d2b6873
Matic Sisko Musil <130707864+MaticSM1@users.noreply.github.com>
2025-06-11 12:42:25 +0200

    izbolsan dockertest

--- orv/dockertest.py
+++ orv/dockertest.py
@@ -1,1 +1,17 @@
-print("docker ok")
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+def run_all_py():
+    subprocess.Popen(['python', 'all.py'])
+
+@app.route('/')
+def home():
+    run_all_py()
+    return "ok"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
